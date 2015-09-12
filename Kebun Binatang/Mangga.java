6cab39bb2984133315cf5cbe048c29b282547896
Nur Muharram <nurmuh033@gmail.com>
2015-09-12 17:23:44 +0700

    Praktikum 5 Kebung Binatang

---
+++ Kebun Binatang/Mangga.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Mangga here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Mangga implements Tumbuhan
+{
+
+public boolean berbuah(){
+   boolean berbuah = true;
+    return berbuah;
+}
+}

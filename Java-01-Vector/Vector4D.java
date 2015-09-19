f57998bc8674a60aec437654ddd564f30a4f7a42
Nur Muharram <nurmuh033@gmail.com>
2015-09-19 17:06:17 +0700

    Praktikum 3

---
+++ Java-01-Vector/Vector4D.java
@@ -0,0 +1,22 @@
+import java.util.Date;
+/**
+ * Write a description of
+ * class Vector4D here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Vector4D extends Vector3D
+{
+    private Date t = new Date();
+    
+    public Vector4D(double x,double y, double z, Date t) {
+    super (x,y,z);
+        this.t=t;
+    }
+    
+    public Date getT(){
+        return t;
+    }
+    
+}
